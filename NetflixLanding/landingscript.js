14b38c96584e4cded53b57efa63744327011e263
Stefan <stefanpopovic5@gmail.com>
2026-02-26 14:33:18 +0100

    New Directory
    Added new directory

---
+++ NetflixLanding/landingscript.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    document.getElementById("panel").style.display = "block";
+}
+function myFunction1() {
+    document.getElementById("panel1").style.display = "block";
+}
+function myFunction2() {
+    document.getElementById("panel2").style.display = "block";
+}
+function myFunction3() {
+    document.getElementById("panel3").style.display = "block";
+}
+function myFunction4() {
+    document.getElementById("panel4").style.display = "block";
+}
+function myFunction5() {
+    document.getElementById("panel5").style.display = "block";
+}
